720efe7d8ecfab583e01b931c602b88a71600ee4
Danilo <danilooalmeida94@gmail.com>
2017-02-23 14:30:13 -0300

    add Registers

---
+++ Registers.v
@@ -0,0 +1,29 @@
+module Registers(input clk,
+input [4:0] rs,
+input [4:0] ra,
+input [4:0] wr,
+input [31:0] writeData,
+input write,
+output [31:0]readDatars,
+output [31:0]readDatara);
+
+
+reg [31:0] registers[0:31];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+endmodule
