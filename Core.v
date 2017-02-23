d422ebd7e2cd929ad9a2db088f63c645675a3725
Danilo <danilooalmeida94@gmail.com>
2017-02-23 13:19:20 -0300

    Program Counter

--- Core.v
+++ Core.v
@@ -1,3 +1,11 @@
+/*
+ MIPS32 Core
+ Designed by: danilo94
+ This module have only one input and is used to contain all MIPS submodules 
+*/
+#include "ProgramCounter.v"
+
+
 module mipsCore(input clk);
 
 
--- ProgramCounter.v
+++ ProgramCounter.v
@@ -1,3 +1,8 @@
+/*
+ Program Counter MIPS32 
+ Designed by: danilo94
+ This module have four inputs and one output used to detect jumps and load instructions to datapath
+*/
 module PC(input clk,
 input [31:0] pc,
 output reg [31:0] newPC, input isJump,input rst);
@@ -11,9 +16,7 @@ if (rst) begin
 end
 
 else begin
-
-
-
+counter <= counter + 4;
 end 
 
 
