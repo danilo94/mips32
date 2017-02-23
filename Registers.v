81e1522d32c3216a063c355b408263f87fb3bff1
Danilo <danilooalmeida94@gmail.com>
2017-02-23 15:04:02 -0300

    Registers Version to test

--- Registers.v
+++ Registers.v
@@ -1,3 +1,12 @@
+/*
+ MIPS32 Registers
+ Designed by: danilo94
+ This module is the internal memory of mips core, your use is necessary to execute and manipulate all data inside processor with the maximum speed
+*/
+
+
+
+
 module Registers(input clk,
 input [4:0] rs,
 input [4:0] ra,
