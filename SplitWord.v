3b873fd0de36f2c3de56a09e01aa0aa15f1c9c4e
Danilo <danilooalmeida94@gmail.com>
2017-02-23 19:37:24 -0300

    Split Word Module

---
+++ SplitWord.v
@@ -0,0 +1,34 @@
+
+/*
+ SplitWord MIPS32 
+ Designed by: danilo94
+This module only split a word into small pieces to feed other modules
+*/
+
+
+
+module splitWord(
+input [31:0] word,
+output [5:0] opcode,
+output [4:0] rs,
+output [4:0] rt,
+output [4:0] rd, 
+output [4:0] shamt,
+output [5:0] funct,
+output [15:0] address
+);
+
+
+
+
+assign op = word[31:26];
+assign rs = word[25:21];
+assign rt = word[20:16];
+assign rd = word [15:11];
+assign shamt = word[10:6];
+assign funct = word[5:0];
+assign address = word[15:0];
+
+
+
+endmodule
