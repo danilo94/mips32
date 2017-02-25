b10e806738ff466409be619a6c7feb3496977e42
Danilo <danilooalmeida94@gmail.com>
2017-02-25 16:40:54 -0300

    Control created

---
+++ Control.v
@@ -0,0 +1,27 @@
+controlUnity(
+input [5:0] opcode,
+output reg PCWrite,
+output reg MemRead,
+output reg MemWrite,
+output reg MemtoReg,
+output reg [1:0] PCSource,
+output reg AluOp,
+output reg AluScrcA,
+output reg [2:0] AluSrcB,
+output reg regWrite,
+output reg regdst;
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+endmodule
